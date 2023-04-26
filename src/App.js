577fc827cf1fd30ef2228b668daa469c6719fba8
hardcod3dd <hardcod3dd@github.com>
2023-04-26 14:26:45 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [prices, setPrices] = useState([]);
+
+  useEffect(() => {
+    const fetchCryptoPrices = async () => {
+      const coins = ['bitcoin', 'ethereum', 'dogecoin','monero', 'ripple', 'cardano', 'solana', 'polkadot'];
+      const response = await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${coins.join(',')}&vs_currencies=usd`);
+      const data = await response.json();
+      const pricesArray = [];
+      for (const coin of coins) {
+        const price = data[coin].usd;
+        pricesArray.push({ coin, price });
+      }
+      setPrices(pricesArray);
+    };
+
+    fetchCryptoPrices();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="prices">
+      <div className="card-container">
+        {prices.map((crypto, index) => (
+          <div className="card" key={index}>
+            <h2>{crypto.coin}</h2>
+            <p>${crypto.price}</p>
+          </div>
+        ))}
+      </div>
+      <div className='footer'>
+        <p>Her sayfa yenilendiğinde fiyatlar güncellenecektir.</p>
+      </div>
     </div>
+    
   );
 }
 
